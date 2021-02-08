de6179d0564c1baddaee92385b496e4d04d72e80
Filmsuphanut <nongfilm2987@gmail.com>
2021-02-09 01:23:19 +0700

    master

--- my-app/src/components/index.vue
+++ my-app/src/components/index.vue
@@ -1,10 +1,13 @@
 <template>
 
-
     <v-container>
       <div>
-        <h1 class = "red"><center>THIS IS TEST SERVER</center></h1>
+        <h1 class="primary"><center>THIS IS TEST SERVER</center></h1>
       </div>
+      
+  <div class="purple lighten-1 text-center">
+    <span class="white--text">Lorem ipsum</span>
+  </div>
   </v-container>
 
 
--- my-app/src/plugins/vuetify.js
+++ my-app/src/plugins/vuetify.js
@@ -1,9 +1,18 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
+//import colors from 'vuetify/lib/util/colors'
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
-    //theme: { dark: true },
-});
+    theme: {
+      themes: {
+        light: {
+          primary: '#00ff', // #E53935
+          secondary: '#00ff', // #FFCDD2
+          accent: '#00ff', // #3F51B5
+        },
+      },
+    },
+  })
 
