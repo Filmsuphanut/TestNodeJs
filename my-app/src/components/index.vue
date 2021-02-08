5281ccb8e13b5eac6fe6bc7aff3bf9105e5d9d78
Filmsuphanut <nongfilm2987@gmail.com>
2021-02-09 01:39:36 +0700

    master

--- my-app/src/components/index.vue
+++ my-app/src/components/index.vue
@@ -1,3 +1,4 @@
+
 <template>
 
     <v-container>
@@ -5,18 +6,25 @@
         <h1 class="primary"><center>THIS IS TEST SERVER</center></h1>
       </div>
       
-      <div>
-        <v-btn>RUN</v-btn>
-      </div>
-
-
       <div class="purple lighten-1 text-center">
         <span class="white--text">Lorem ipsum</span>
       </div>
+
+      <div v-if="Math.random() > 0.5">
+        Now you see me
+      </div>
+      <div v-else>
+        Now you don't
+      </div>
+
+
   </v-container>
 
 
 </template>
+
+
+
 
 
 <script>
