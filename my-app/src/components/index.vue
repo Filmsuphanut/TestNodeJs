3ba8fa5c2cf1364cf53919886c6c825b9da3289d
Filmsuphanut <nongfilm2987@gmail.com>
2021-02-11 01:48:36 +0700

    script test

--- my-app/src/components/index.vue
+++ my-app/src/components/index.vue
@@ -1,13 +1,18 @@
 
 <template>
+  <nav>
 
-    <v-container>
+      <v-toolbar app dark>
+        <v-toolbar-side-icon @click="drawer=!drawer">
+            click me !! 
+        </v-toolbar-side-icon>
+        <v-toolbar-title class="headline">
+          <span>Hello world</span>
+        </v-toolbar-title>
+      </v-toolbar>
 
-      
-<div id="app">
-  {{ message }}
-</div>
 
+      <v-container>
       <div>
         <h1 class="primary"><center>THIS IS TEST SERVER</center></h1>
       </div>
@@ -16,20 +21,51 @@
         <span class="white--text">Lorem ipsum</span>
       </div>
 
+        <v-card
+          v-click-outside="onClickOutside"
+          :color="active ? 'primary' : undefined"
+          :dark="active"
+          class="mx-auto"
+          height="256"
+          rounded="xl"
+          width="256"
+          @click="active = true"
+        >
+          <div class="text-h6 text-md-h4 fill-height d-flex align-center justify-center">
+            {{ active ? 'Click Outside' : 'Click Me' }}
+          </div>
+        </v-card>
+ 
+      </v-container>
+    
+
+     <v-navigation-drawer app v-model="drawer">
+        <p>Filmsuphanut</p>
+      </v-navigation-drawer>
 
 
-
-  </v-container>
-
-
+</nav>
 </template>
 
 
-
-
-
-<script>
+ <script>
   export default {
+    data: () => (
+    {
+      active: false,
+      drawer: true,
+    }
+    ),
 
+    methods: {
+      onClickOutside () {
+        this.active = false
+      },
+      
+
+    },
   }
 </script>
+
+
+
